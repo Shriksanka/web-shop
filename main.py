d6724018be4fecfb0b11dc8bfb99db8d66a006fd
Shriksanka <92088133+Shriksanka@users.noreply.github.com>
2023-08-17 14:17:27 +0200

    Commited

---
+++ main.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot('6543405819:AAFUaqHH9z8Q4jhOp1V2iWI6JQG7ZOE-i2A')
+dp = Dispatcher(bot)
+
+
+async def on_startup(_):
+    print('Бот успешно запущен!')
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='https://shriksanka.github.io/web-shop/')))
+    await message.answer('Привет мой друг', reply_markup=markup)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
